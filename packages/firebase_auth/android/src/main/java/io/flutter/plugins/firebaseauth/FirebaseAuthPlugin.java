bf33539b9395cf15c0182a217073ec0f440b99df
ko2ic <ko2ic.dev@gmail.com>
2018-08-03 09:40:27 +0900

    Adding support for FirebaseUser.unlink(providerId)
    * Unlink an auth provider from a user account

--- packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
+++ packages/firebase_auth/android/src/main/java/io/flutter/plugins/firebaseauth/FirebaseAuthPlugin.java
@@ -121,6 +121,8 @@ public class FirebaseAuthPlugin implements MethodCallHandler {
         break;
       case "linkWithTwitterCredential":
         handleLinkWithTwitterCredential(call, result, getAuth(call));
+      case "unlink":
+        handleUnlink(call, result, getAuth(call));
         break;
       case "linkWithGithubCredential":
         handleLinkWithGithubCredential(call, result, getAuth(call));
@@ -458,6 +460,15 @@ public class FirebaseAuthPlugin implements MethodCallHandler {
     String token = arguments.get("token");
     firebaseAuth
         .signInWithCustomToken(token)
+        .addOnCompleteListener(new SignInCompleteListener(result));
+  }
+
+  private void handleUnlink(MethodCall call, final Result result, FirebaseAuth firebaseAuth) {
+    Map<String, String> arguments = call.arguments();
+    String providerId = arguments.get("providerId");
+    firebaseAuth
+        .getCurrentUser()
+        .unlink(providerId)
         .addOnCompleteListener(new SignInCompleteListener(result));
   }
 
